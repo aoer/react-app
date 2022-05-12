7dda4b9dbce5a6bb113a0204e05313abb0794eac
aoer <not@real.com>
2022-05-12 05:31:09 +0300

    Practised array handling and mapping.

--- src/TableExercise/TableData.js
+++ src/TableExercise/TableData.js
@@ -2,10 +2,67 @@ class TableData {
     constructor(rowCount, columnCount) {
         this.rowCount = rowCount;
         this.columnCount = columnCount;
+        this.header = [];
+        this.rows = [];
+        this.initDefaultTable();
     };
 
-    getData() {
-        return this.rowCount + ' ' + this.columnCount;
+    initDefaultTable() {
+        for(let c=0; c<this.columnCount; c++) {
+            this.header[c] ='Default Header ' + c;
+        }
+
+        for(let r=0;r<this.rowCount; r++) {
+            let row = [];
+            for(let c=0; c<this.columnCount; c++) {
+                row[c] ='Default Cell ' + r + ' ' + c;
+            }
+            
+            this.rows[r] = row; 
+        }
+
+    }
+
+    getHeaderData() {
+        return this.header;
+    }
+
+    getRowData() {
+        return this.rows;
+    }
+
+    setCell(row, column, value) {
+        if (!this.isValidRow(row) || !this.isValidColumn(column))
+            return;
+        this.rows[row][column] = value;
+    }
+
+    setRow(row, value) {
+        if (!this.isValidRow(row))
+            return;
+        for (let i=0; i < this.rows[row].length; i++) {
+            this.rows[row][i] = value;
+        }       
+    }
+
+    setColumn(column, value) {
+        if (!this.isValidColumn(column))
+            return;
+        for (let r=0; r < this.rows.length; r++) {
+            this.rows[r][column] = value;
+        }
+    }
+
+    isValidRow(rowNumber) {
+        if (rowNumber < 0 || rowNumber >= this.rowCount)
+            return false;
+        return true;
+    }
+
+    isValidColumn(columnNumber) {
+        if (columnNumber < 0 || columnNumber >= this.columnCount)
+            return false;
+        return true;
     }
 }
 
--- src/TableExercise/TableExercise.js
+++ src/TableExercise/TableExercise.js
@@ -2,27 +2,38 @@ import TableData from "./TableData";
 import "./TableExercise.css";
 
 function TableExercise() {
-    const tData = new TableData(3, 4);
+    const tData = new TableData(6, 4);
+    tData.setCell(2, 2, 'Altered Cell!');
+    tData.setRow(0, 'Altered Rows!');
+    tData.setColumn(1, 'Altered Columns!');
 
-    let buildTable = () => {
+    let buildTable = (rowCount, columnCount) => {
+        const headerHTML = (hData) => hData.map((hItem) => <th>{hItem}</th>);
+        const headerHTML_V2 = () => tData.getHeaderData().map((hItem) => <th>{hItem}</th>);
+        const contentHTML = () => {
+            const cData = tData.getRowData();
+            return cData.map((rItem) => <tr>{rItem.map((cItem) => <td>{cItem}</td>)}</tr>);
+        }
+        const contentHTML_V2 = () => {
+            return rowHTML(tData.getRowData());
+        }
+        
+        const rowHTML = (rData) => rData.map((rItem) => <tr>{columnHTML(rItem)}</tr>);
+        const columnHTML = (cData) => cData.map((cItem) => <td>{cItem}</td>);
+    
         return (
             <table>
-                <tr>
-                    <th>Default Header 1</th>
-                </tr>
-                <tr>
-                    <td>Default Data 1</td>
-                </tr>
+                {headerHTML_V2()}
+                {contentHTML_V2()}
             </table>
         )
     }
     
     return (
         <div id='table-exercise'>
-            {tData.getData()}
             {buildTable()}
         </div>
-    );
+    )
 }
 
 export default TableExercise;
